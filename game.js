1be6f65caae5b01a54641632e759f4e7c9a7c6be
marcobi24 <mbernaildeonso@gmail.com>
2022-09-14 12:33:05 -0500

    diseño responsivo del teclado

--- game.js
+++ game.js
@@ -1,8 +1,17 @@
 let btns = document.querySelectorAll(".tecla")
+btns[0].onmouseover = () => {   console.log("mouseover") }
 console.log(btns);
-btns.forEach(btn => {
-    btn.onclick = (e)=>{
-        console.log(e);
-        console.log(btn.value);
-    } 
-});
+window.onkeydown = (e) => {
+    let teclaPulsada = e.key
+    console.log(teclaPulsada);
+    btns.forEach(btn => {
+        if (btn.value == teclaPulsada) {
+            console.log("pulsaste la tecla " + btn)
+            console.log(btn);
+
+                const eventHover = new Event("click");
+            console.log(eventHover);
+            btn.dispatchEvent(eventHover);
+        }
+    });
+};
