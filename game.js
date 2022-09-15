88697ca4cd2beaf7ac37f5d75b60d20152a5dfde
Marco Berna <mbernaildeons@gmail.com>
2022-09-15 00:44:04 -0500

    ya se puede verificar si ha ganado o ha perdido y ya se puede escribir la palabra

--- game.js
+++ game.js
@@ -1,9 +1,224 @@
 let btns = document.querySelectorAll(".tecla")
-let arrayValues =[]
+let arrayValues = []
 btns.forEach(btn => {
     arrayValues.push(btn.value)
 });
-// guardar todos los valores en un array y cuando quiero verficar el index de la letra el array lo convierto en una palabra y obtengo el charAt y lo pase al array de los elmeento y le pongo un style y con keyUp igual
+// canvas para dibujar el muñeco
+const canvas = document.getElementById("screen-game")
+let context = canvas.getContext("2d");
+// algoritmo-1 => guarda en variables o objetos las posiciones 
+//  las posiciones de cada dibujo
+// y despues iterarlas de acuerdo a numeros de fallos
+
+
+// parametros para dibujar en el
+const piso = {
+    inicialX: 50,
+    inicialY: 350,
+    finalX: 350,
+    finalY: 350,
+}
+const tubo = {
+    inicialX: 50,
+    inicialY: 50,
+    finalX: 50,
+    finalY: 350,
+}
+const techo = {
+    inicialX: 50,
+    inicialY: 50,
+    finalX: 220,
+    finalY: 50,
+}
+const horca = {
+        inicialX: 220,
+        inicialY: 50,
+        finalX: 220,
+        finalY: 100,
+    }
+const cuerpo = {
+    inicialX: 220,
+    inicialY: 180,
+    finalX: 220,
+    finalY: 260,
+}
+const brazoDerecho = {
+    inicialX: 220,
+    inicialY: 180,
+    finalX: 260,
+    finalY: 250,
+}
+const cabeza = {
+    inicialX: 220,
+    inicialY: 140,
+}
+const brazoIzquierdo = {
+    inicialX: 220,
+    inicialY: 180,
+    finalX: 180,
+    finalY: 250,
+}
+const pataDerecha = {
+    inicialX: 220,
+    inicialY: 260,
+    finalX: 260,
+    finalY: 320,
+}
+const pataIzquierda = {
+
+    inicialX: 220,
+    inicialY: 260,
+    finalX: 180,
+    finalY: 320,
+}
+
+const arrayPiezas = [tubo, techo, horca, cabeza, cuerpo, brazoDerecho, brazoIzquierdo, pataDerecha, pataIzquierda]
+
+init()
+
+let fallos = 0;
+let palabraOrigin = "marco"
+let palabra = palabraOrigin.split("");
+let letrasAcertadas = []
+let letrasNoAcertadas = []
+let letrasPalabraEscrita = ["-", "-", "-", "-", "-"]
+let arrayPalabraCorrectas = document.querySelectorAll(".letra-correcta")
+let contenedorPalabraIncorrectas = document.getElementById("contenedor-palabras-incorrectas")
+console.log(letrasPalabraEscrita);
+window.addEventListener("keyup", paint, event)
+function paint(event) {
+    let teclaPulsada = event.key
+
+
+    let verificacionDeLaTecla = verificacionYProcesamientoDeLaTeclaPulsada(teclaPulsada)
+    if (verificacionDeLaTecla) {
+        console.log("%cGANASTE!!", "font-size: 24px;");
+        window.removeEventListener("keyup", paint)
+    } else if (verificacionDeLaTecla == false) {
+        console.log("%cPERDISTE!!", "font-size: 24px;");
+        window.removeEventListener("keyup", paint)
+
+    }
+
+
+
+    console.log(letrasAcertadas);
+    console.log("acertadas");
+    console.log(letrasNoAcertadas);
+    console.log("no acertadas");
+    // console.log(palabraEscrita);
+
+
+}
+
+
+function verificacionYProcesamientoDeLaTeclaPulsada(teclaPulsada) {
+    console.log(arrayPalabraCorrectas);
+
+    if (palabra.includes(teclaPulsada)) {
+        let posicionDeLaTeclaPulsada = palabra.indexOf(teclaPulsada)
+        arrayPalabraCorrectas[posicionDeLaTeclaPulsada].innerHTML = teclaPulsada
+                letrasAcertadas.push(teclaPulsada)
+                if (letrasAcertadas.join("") == palabra.join("")) {
+                    return true
+                }
+
+
+
+    } else {
+        if (!letrasNoAcertadas.includes(teclaPulsada)) {
+            letrasNoAcertadas.push(teclaPulsada)
+            agregarLetraIncorrecta(teclaPulsada)
+        }
+        let pieza = arrayPiezas[fallos]
+        if (pieza == undefined) {
+            return false
+        }
+        if (fallos == 3) {
+            dibujarCabeza(pieza.inicialX, pieza.inicialY)
+        } else {
+            dibujarLinea(pieza.inicialX, pieza.inicialY, pieza.finalX, pieza.finalY)
+        }
+        fallos++
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function agregarLetraIncorrecta(teclaPulsada) {
+    let div = document.createElement("div")
+    div.classList.add("letra-incorrecta")
+    div.innerHTML = teclaPulsada
+    contenedorPalabraIncorrectas.appendChild(div)
+}
+function dibujarLinea(inicialX, inicialY, finalX, finalY) {
+    context.beginPath();
+
+    context.lineWidth = 6;
+    context.moveTo(inicialX, inicialY);
+    context.lineTo(finalX, finalY);
+    context.lineCap = "round";
+    context.stroke();
+
+    context.closePath();
+}
+
+
+function dibujarCabeza(x, y) {
+    context.beginPath();
+    context.strokeStyle = 'black';
+    context.lineWidth = 8;
+
+    context.arc(x, y, 40, Math.PI / 180 * 0, Math.PI / 180 * 360);
+
+    context.stroke();
+
+
+    context.closePath();
+
+}
+function init() {
+    dibujarLinea(piso.inicialX, piso.inicialY, piso.finalX, piso.finalY) // piso
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 window.onkeydown = (e) => {
     if (!arrayValues.includes(e.key)) {
         return
@@ -11,7 +226,7 @@ window.onkeydown = (e) => {
     let indiceDeLaTecla = arrayValues.indexOf(e.key)
     btns[indiceDeLaTecla].style.backgroundColor = "orange"
 };
-window.onkeyup = (e)=>{
+window.onkeyup = (e) => {
     if (!arrayValues.includes(e.key)) {
         return
     }
@@ -19,8 +234,8 @@ window.onkeyup = (e)=>{
     btns[indiceDeLaTecla].style.backgroundColor = "yellow"
 
 }
-window.onfocus = ()=>{
-    btns.forEach((btn)=>{
+window.onfocus = () => {
+    btns.forEach((btn) => {
         btn.style.backgroundColor = "yellow"
     })
 }
