d660a81ab929e3148d8f266e1db988d80ca5e0d8
marcobi24 <mbernaildeonso@gmail.com>
2022-09-25 19:32:15 -0500

    he diseñado los botones y el titulo de la pagina de elegirModo.html

--- elegirModo.js
+++ elegirModo.js
@@ -4,3 +4,20 @@ btnRetro.onclick = ()=> {
     window.location = window.origin + "/"   + "index.html"
     
 };
+const btnMasModos = $("mas-modos")
+const contenedorMasModos = $("contenedor-mas-modos");
+btnMasModos.onclick = ()=> { 
+     if (btnMasModos.dataset.open == "false") {
+         contenedorMasModos.style.maxHeight = "400px"
+         btnMasModos.dataset.open = "true"
+         btnMasModos.childNodes[0].style.transform = "rotate(180deg)"
+     }else { 
+        contenedorMasModos.style.maxHeight = "0px"
+        btnMasModos.dataset.open = "false"
+        btnMasModos.childNodes[0].style.transform = "rotate(0deg)"
+    
+    }
+
+    console.log(btnMasModos.childNodes[0]);    
+}
+
