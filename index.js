33f0cc9cdaea098839cfdb35284b6e133aea5594
Marco Berna <mbernaildeons@gmail.com>
2022-10-19 21:47:14 -0500

    mostrar mensajes

--- index.js
+++ index.js
@@ -5,25 +5,32 @@ const express = require('express')
 const path = require('path')
 const body_parser = require('body-parser')
 const app = express().use(body_parser.json())
-var xhub = require('express-x-hub');
-app.use(xhub({ algorithm: 'sha1', secret: "08daf43ab4a670e4178e2922f2af9c2b" }));
+var xhub = require('express-x-hub')
+app.use(xhub({ algorithm: 'sha1', secret: '08daf43ab4a670e4178e2922f2af9c2b' }))
 // const request = require("request")
 app.use(express.urlencoded({ extended: false }))
 app.use('/', express.static(path.join(__dirname, 'public')))
 // app.get("/chat", (req,res)=>{
 //     res.send("hoola")
 // })
-app.post('/',require("./enviarMensajeWhatsapp"))
+let mensajes = ["hola", "como estas", "prueba"]
+app.post('/', require('./enviarMensajeWhatsapp'))
 // app.use('/', require('./routes'))
-app.get('/facebook', require('./verificarTokenWhatsapp'))
-app.post('/facebook', (req, res) => {
+app.get('/facebook', require('./verificarTokenWhatsapp')) // esta funcion verifica el token
+app.post('/facebook', (req, res) => { // esta funcion espera el mensaje de whatsap 
   console.log('Facebook request body')
-  console.log(JSON.stringify(req.body));
+  console.log(JSON.stringify(req.body))
+  mensajes.unshift(req.body.entry[0].changes[0].value.messages[0].text.body)
 
-
-  console.log('request header X-Hub-Signature validated');
+  console.log('request header X-Hub-Signature validated')
   // Process the Facebook updates here
-    res.sendStatus(200);
+  res.sendStatus(200)
+})
+
+app.get('/chat', (req, res) => {
+  console.log(req)
+  console.log('REQUEST!!!')
+  res.send("<pre>" + JSON.stringify(mensajes, null, 2) + "</pre>")
 })
 app.listen(process.env.PORT || 3000)
 // app.listen(, () => console.log('Se prendio la máquina'))
