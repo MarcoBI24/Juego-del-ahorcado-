f83cb64b7325ec55fca737d3c2b163c744e96a41
Marco Berna <mbernaildeons@gmail.com>
2022-10-20 23:23:43 -0500

    Reconocoinedo comandos

--- enviarMensaje.js
+++ enviarMensaje.js
@@ -1,22 +1,32 @@
-
 const fetch = require('node-fetch')
 
-
-module.exports = (nameTemplate) => {
+module.exports = (nameTemplate, mensaje) => {
   const token =
     'EAAHWaNPjDy4BAH1Nmkj7r5NyfJatfnlN60J5iFedQcAHOS32zQknlWGPz2G1zwGK8xR4d1WwihLyUsyPNxjZCRMhWuNOf00y2ujHskjDvxcK1t9OQwS8JPkm7q7ZBaZBDTHFr05zfwtLzS8bztEioeCFLaxcM2uShL0zQL3IKqsNZCHGjNTecLZA1GvdAKqoPzdAp0rDeQqvNfQ7pUke0'
   const headers = {
     Authorization: `Bearer ${token}`,
     'Content-Type': 'application/json'
   }
-  const options = {
-    messaging_product: 'whatsapp',
-    to: '51900866170',
-    type: 'template',
-    template: {
-      name: nameTemplate,
-      language: {
-        code : "en_US"
+  let options = {}
+  if (nameTemplate === null) {
+    options = {
+      messaging_product: 'whatsapp',
+      to: '51900866170',
+      type: 'template',
+      template: {
+        name: nameTemplate,
+        language: {
+          code: 'en_US'
+        }
+      }
+    }
+  } else if (mensaje !== null || mensaje !== undefined) {
+    options = {
+      messaging_product: 'whatsapp',
+      to: '51900866170',
+      type: 'text',
+      text: {
+        body : mensaje
       }
     }
   }
--- verificarElMensajeRecibido.js
+++ verificarElMensajeRecibido.js
@@ -1,20 +1,104 @@
 const router = require('express').Router()
-const { EnvironmentContext } = require('twilio/lib/rest/serverless/v1/service/environment')
-const enviarMensaje = require("./enviarMensaje")
+const enviarMensaje = require('./enviarMensaje')
+
+let palabraSecreta = "marco"
+let mensaje = ''
+let jugando = false
+let errores = 0
+let mensajeGuiones = ""
+let mensajeHombre = ""
+const IMAGENES_AHORCADO = [`
+
+   +---+
+   |   |
+       |
+       |
+       |
+       |
+
+=========`, `
+
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========`, `
+
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========`, `
+
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========`, `
+
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========`, `
+
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========`, `
+
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========`]
+router.route('/facebook').post((req, res) => {
+  // esta funcion espera el mensaje de whatsap
+  mensaje = req.body.entry[0].changes[0].value.messages[0].text.body
+  if (mensaje === '/opciones' && jugando == false) {
+    enviarMensaje('mostrar_opciones')
+  }
+  
+  if (mensaje === "Jugar") {
+    jugando = true
+    enviarMensaje('como_jugar')
+  }
+  if (jugando == true) {
+    // tener la palabra secreta
+    // el largo de la palabra secreta
+    // hacer un mensaje con guiones del largo de la palabra secreta
+    // hacer un mensaje con el dibujo en la posicion del numero de errores
+    // recibir la letra
+    mensajeHombre = IMAGENES_AHORCADO[errores]
+    palabraSecreta.split("").forEach(letra => {
+      mensajeGuiones+="_"
+    });
+    mensajeGuiones = mensajeGuiones.join(" ")
+    mensaje = mensajeHombre + "\n\n" + mensajeGuiones
+    enviarMensaje(null,mensaje)
 
 
-router.route("/facebook").post((req, res) => { // esta funcion espera el mensaje de whatsap 
-    let mensaje = ""
 
-    console.log('Solicitud de Facebook')
-    console.log(JSON.stringify(req.body))
-    mensaje = req.body.entry[0].changes[0].value.messages[0].text.body
-    if (mensaje === "/opciones") {
-        enviarMensaje("mostrar_opciones")
-    }
-    res.sendStatus(200)
-  })
+  }  
+  
 
 
+
+
+  res.sendStatus(200)
+})
 
 module.exports = router
