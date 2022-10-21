7e51db7d21d2d0f97f2e84f5f8b012b5cb8fa552
Marco Berna <mbernaildeons@gmail.com>
2022-10-20 23:42:58 -0500

    Reconocoinedo comandos

--- verificarElMensajeRecibido.js
+++ verificarElMensajeRecibido.js
@@ -67,33 +67,39 @@ const IMAGENES_AHORCADO = [`
 =========`]
 router.route('/facebook').post((req, res) => {
   // esta funcion espera el mensaje de whatsap
-  mensaje = req.body.entry[0].changes[0].value.messages[0].text.body
-  if (mensaje === '/opciones' && jugando == false) {
-    enviarMensaje('mostrar_opciones',null)
-  }
+  if (req.body.entry[0].changes[0].value.messages[0].text.body) {
+    
+    mensaje = req.body.entry[0].changes[0].value.messages[0].text.body
+    if (mensaje === '/opciones' && jugando == false) {
+      enviarMensaje('mostrar_opciones',null)
+    }
+    
+    if (mensaje === "Jugar") {
+      jugando = true
+      enviarMensaje('como_jugar',null)
+    }
+    if (mensaje === "/salir") {
+      jugando = false
+    }
+    if (jugando == true) {
+      // tener la palabra secreta
+      // el largo de la palabra secreta
+      // hacer un mensaje con guiones del largo de la palabra secreta
+      // hacer un mensaje con el dibujo en la posicion del numero de errores
+      // recibir la letra
+      mensajeHombre = IMAGENES_AHORCADO[errores]
+      palabraSecreta.split("").forEach(letra => {
+        mensajeGuiones+="_"
+      });
+      mensajeGuiones = mensajeGuiones.join(" ")
+      mensaje = mensajeHombre + "\n\n" + mensajeGuiones
+      enviarMensaje(null,mensaje)
   
-  if (mensaje === "Jugar") {
-    jugando = true
-    enviarMensaje('como_jugar',null)
-  }
-  if (jugando == true) {
-    // tener la palabra secreta
-    // el largo de la palabra secreta
-    // hacer un mensaje con guiones del largo de la palabra secreta
-    // hacer un mensaje con el dibujo en la posicion del numero de errores
-    // recibir la letra
-    mensajeHombre = IMAGENES_AHORCADO[errores]
-    palabraSecreta.split("").forEach(letra => {
-      mensajeGuiones+="_"
-    });
-    mensajeGuiones = mensajeGuiones.join(" ")
-    mensaje = mensajeHombre + "\n\n" + mensajeGuiones
-    enviarMensaje(null,mensaje)
-
-
-
-  }  
   
+  
+    }  
+    
+  }
 
 
 
