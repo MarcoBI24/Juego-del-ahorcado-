7fd7b8db459e4e5f38f97f8c4226896be6c73526
Marco Berna <mbernaildeons@gmail.com>
2022-10-23 23:37:59 -0500

    Jugar pulsando el boton de jugar !

--- verificarElMensajeRecibido.js
+++ verificarElMensajeRecibido.js
@@ -1,13 +1,14 @@
 const router = require('express').Router()
 const enviarMensaje = require('./enviarMensaje')
 
-let palabraSecreta = "marco"
+let palabraSecreta = 'marco'
 let mensaje = ''
 let jugando = false
 let errores = 0
-let mensajeGuiones = ""
-let mensajeHombre = ""
-const IMAGENES_AHORCADO = [`
+let mensajeGuiones = ''
+let mensajeHombre = ''
+const IMAGENES_AHORCADO = [
+  `
 
    +---+
    |   |
@@ -16,7 +17,8 @@ const IMAGENES_AHORCADO = [`
        |
        |
 
-=========`, `
+=========`,
+  `
 
   +---+
   |   |
@@ -24,7 +26,8 @@ const IMAGENES_AHORCADO = [`
       |
       |
       |
-=========`, `
+=========`,
+  `
 
   +---+
   |   |
@@ -32,7 +35,8 @@ const IMAGENES_AHORCADO = [`
   |   |
       |
       |
-=========`, `
+=========`,
+  `
 
   +---+
   |   |
@@ -40,7 +44,8 @@ const IMAGENES_AHORCADO = [`
  /|   |
       |
       |
-=========`, `
+=========`,
+  `
 
   +---+
   |   |
@@ -48,7 +53,8 @@ const IMAGENES_AHORCADO = [`
  /|\  |
       |
       |
-=========`, `
+=========`,
+  `
 
   +---+
   |   |
@@ -56,7 +62,8 @@ const IMAGENES_AHORCADO = [`
  /|\  |
  /    |
       |
-=========`, `
+=========`,
+  `
 
   +---+
   |   |
@@ -64,41 +71,58 @@ const IMAGENES_AHORCADO = [`
  /|\  |
  / \  |
       |
-=========`]
-// en req me envia la data
-// en res le envio el status
+=========`
+][
+  // en req me envia la data
+  // en res le envio el status
+  {
+    context: {
+      from: '15550877052',
+      id: 'wamid.HBgLNTE5MDA4NjYxNzAVAgARGBI4MDVCMDJGRDk2QTNDNDVEMzAA'
+    },
+    from: '51900866170',
+    id:
+      'wamid.HBgLNTE5MDA4NjYxNzAVAgASGCBFMzY2QzZDODFGQTJFODFDRjAzRDJGRTA2QjZFNDFFRgA=',
+    timestamp: '1666585809',
+    type: 'button',
+    button: { payload: 'Jugar', text: 'Jugar' }
+  }
+]
+
 router.route('/facebook').post(async (req, res) => {
   // esta funcion espera el mensaje de whatsap
-  if ( req.body.entry[0].changes[0].value.messages !== undefined && req.body.entry[0].changes[0].value.messages[0] !== undefined) {
-    console.log(req.body.entry[0].changes[0].value.messages)
-    mensaje = req.body.entry[0].changes[0].value.messages[0].text.body
-    console.log(mensaje + "MENSAJE RECIBIDO");
-    if (mensaje === "Hola" && jugando === false) {
-      let nombreUser = req.body.entry[0].changes[0].value.contacts[0].profile.name
-      await enviarMensaje(null,`Hola ${nombreUser}, ¿Qué tal?`)
+  const REQ = req.body.entry[0].changes[0].value.messages
+  if (REQ !== undefined && REQ[0] !== undefined) {
+    console.log(REQ)
+    mensaje = REQ[0].text.body
+    console.log(mensaje + 'MENSAJE RECIBIDO')
+    if (mensaje === 'Hola' && jugando === false) {
+      let nombreUser =
+        req.body.entry[0].changes[0].value.contacts[0].profile.name
+      await enviarMensaje(null, `Hola ${nombreUser}, ¿Qué tal?`)
       res.sendStatus(200)
-      mensaje = ""
-      return 
-    }
-    if (mensaje === '/opciones' && jugando == false) {
-      await enviarMensaje('mostrar_opciones',null)
-      mensaje  = ""
-    }else if (mensaje === '/opciones' && jugando == true) {
-      await enviarMensaje(null,'Escribe /salir para abandonar el juego ')
-      res.sendStatus(200)
-      mensaje = ""
+      mensaje = ''
       return
     }
-    
-    if (mensaje === "Jugar" && jugando == false) {
-      jugando = true
-      await enviarMensaje('como_jugar',null)
-      mensaje = ""
+    if (mensaje === '/opciones' && jugando == false) {
+      await enviarMensaje('mostrar_opciones', null)
+      mensaje = ''
+    } else if (mensaje === '/opciones' && jugando == true) {
+      await enviarMensaje(null, 'Escribe /salir para abandonar el juego ')
+      res.sendStatus(200)
+      mensaje = ''
+      return
     }
-    if (mensaje === "/salir" && jugando == true) {
+
+    if ((mensaje === 'Jugar' && jugando == false) || (REQ[0].button.payload == "Jugar" && jugando == false) ) {
+      jugando = true
+      await enviarMensaje('como_jugar', null)
+      mensaje = ''
+    }
+    if (mensaje === '/salir' && jugando == true) {
       jugando = false
-      await enviarMensaje(null,"Saliste del juego")
-      mensaje = ""
+      await enviarMensaje(null, 'Saliste del juego')
+      mensaje = ''
     }
     if (jugando == true) {
       // tener la palabra secreta
@@ -107,23 +131,18 @@ router.route('/facebook').post(async (req, res) => {
       // hacer un mensaje con el dibujo en la posicion del numero de errores
       // recibir la letra
       mensajeHombre = IMAGENES_AHORCADO[errores]
-      palabraSecreta.split("").forEach(letra => {
-        mensajeGuiones+="_"
-      });
-      mensajeGuiones = mensajeGuiones.split("")
-      mensajeGuiones = mensajeGuiones.join(" ")
-      mensaje = mensajeHombre + "\n\n" + mensajeGuiones
-     await enviarMensaje(null,mensaje)
-      mensaje = ""
-  
-  
-    }  
-    
-  }else{
-    console.log("El mensaje se ha enviado,entregado o leido");
+      palabraSecreta.split('').forEach(letra => {
+        mensajeGuiones += '_'
+      })
+      mensajeGuiones = mensajeGuiones.split('')
+      mensajeGuiones = mensajeGuiones.join(' ')
+      mensaje = mensajeHombre + '\n\n' + mensajeGuiones
+      await enviarMensaje(null, mensaje)
+      mensaje = ''
+    }
+  } else {
+    console.log('El mensaje se ha enviado,entregado o leido')
   }
-
-
 
   res.sendStatus(200)
 })
