9cd4249a4162b1d1017f8ec79545a218209748e9
Marco Berna <mbernaildeons@gmail.com>
2022-10-25 22:00:50 -0500

    Arc-ASCII dando forma

--- verificarElMensajeRecibido.js
+++ verificarElMensajeRecibido.js
@@ -19,12 +19,12 @@ for (let i = 0; i < arrPalabraSecreta.length; i++) {
   mensajeGuiones += '_'
 }
 const IMAGENES_AHORCADO = [
-  `\t\t  +---+
-\t\t  |     |
-\t\t        |
-\t\t        |
-\t\t        |
-\t\t        |
+  `\t\t\t\t+---+
+\t\t  |\t\t|
+\t\t   \t\t|
+\t\t   \t\t|
+\t\t   \t\t|
+\t\t   \t\t|
 \t\t=========`, `
 \t\t  +---+
 \t\t  |   |
