21fbf2dca5b4f4e275e288c32f979a2f3c3ef370
Marco Berna <mbernaildeons@gmail.com>
2022-11-05 23:43:01 -0500

    Envio de mensajes

--- enviarMensajeWhatsapp.js
+++ enviarMensajeWhatsapp.js
@@ -3,12 +3,15 @@ const enviarMensaje = require('./enviarMensaje')
 router.route('/').post(async (req, res) => {
   const { usuario, numero, correo } = req.body
   console.log(usuario)
+  console.log(numero)
+  console.log(correo)
   await enviarMensaje('hello_world', null, numero)
   await enviarMensaje(
     null,
     `¡Hey ${usuario}! Bienvenido a The HangGame.`,
     numero
   )
+  res.sendStatus(200)
 })
 
 module.exports = router
