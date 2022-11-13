3a54bed8b855560f1246450fbe5746b36f45f257
Marco Berna <mbernaildeons@gmail.com>
2022-11-13 16:52:41 -0500

    Validar si existe un usuario para mostrar los botones de registrarse o logear.

--- public/modal.js
+++ public/modal.js
@@ -1,38 +1,32 @@
-const $$ = (id) => {
-    return document.getElementById(id)
+const $$ = id => {
+  return document.getElementById(id)
 }
-let modalLink = $$("modal-link")
-let btnMenu = $$("btn-menu")
-let contenedorModal = $$("modal")
-let btnCloseModal = $$("close-modal")
-let btnInicio = $$("btn-irAInicio")
+let modalLink = $$('modal-link')
+let btnMenu = $$('btn-menu')
+let contenedorModal = $$('modal')
+let btnCloseModal = $$('close-modal')
+let btnInicio = $$('btn-irAInicio')
 if (btnInicio) {
-    btnInicio.onclick = ()=>{
-        location.href = location.origin + "/" + "index.html"
-    }
-      
+  btnInicio.onclick = () => {
+    location.href = location.origin + '/' + 'index.html'
+  }
 }
-btnCloseModal.onclick = ()=>{
-
-    let heigthModal = contenedorModal.clientHeight;
-    contenedorModal.style.top = `-${heigthModal}px`
-    setTimeout(() => {
-        
-        modalLink.disabled = true
-        
-    }, 300);
+function cerrarModal () {
+  let heigthModal = contenedorModal.clientHeight
+  contenedorModal.style.top = `-${heigthModal}px`
+  setTimeout(() => {
+    modalLink.disabled = true
+  }, 300)
 }
-btnMenu.onclick = ()=>{
-    modalLink.disabled = false
-    setTimeout(() => {
-        let heigthModal = contenedorModal.clientHeight;
-        window.scrollBy(0,-window.scrollY)
-        // el translate Y es -50vh - la mitad del alto del modal
-        contenedorModal.style.top = `calc(50vh - ${heigthModal / 2}px)`
-
-    }, 200);
-    
+function abrirModal () {
+  modalLink.disabled = false
+  setTimeout(() => {
+    let heigthModal = contenedorModal.clientHeight
+    window.scrollBy(0, -window.scrollY)
+    // el translate Y es -50vh - la mitad del alto del modal
+    contenedorModal.style.top = `calc(50vh - ${heigthModal / 2}px)`
+  }, 200)
 }
-window.onscroll = ()=>{
-
-}
+btnCloseModal.onclick = cerrarModal
+btnMenu.onclick = abrirModal
+window.onscroll = () => {}
