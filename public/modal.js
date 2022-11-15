cdb94682baf3c86448e346272d742562709ac651
Marco <mbernaildeonso@gmail.com>
2022-11-14 21:07:16 -0500

    bug de modal

--- public/modal.js
+++ public/modal.js
@@ -1,32 +1,35 @@
-const $$ = id => {
-  return document.getElementById(id)
-}
-let modalLink = $$('modal-link')
-let btnMenu = $$('btn-menu')
-let contenedorModal = $$('modal')
-let btnCloseModal = $$('close-modal')
-let btnInicio = $$('btn-irAInicio')
+const $$ = (id) => {
+  return document.getElementById(id);
+};
+let modalLink = $$("modal-link");
+let btnMenu = $$("btn-menu");
+let contenedorModal = $$("modal");
+let btnCloseModal = $$("close-modal");
+let btnInicio = $$("btn-irAInicio");
 if (btnInicio) {
   btnInicio.onclick = () => {
-    location.href = location.origin + '/' + 'index.html'
-  }
+    location.href = location.origin + "/" + "index.html";
+  };
 }
-function cerrarModal () {
-  let heigthModal = contenedorModal.clientHeight
-  contenedorModal.style.top = `-${heigthModal}px`
+function cerrarModal() {
+  let heigthModal = contenedorModal.clientHeight;
+  contenedorModal.style.top = `-${heigthModal}px`;
   setTimeout(() => {
-    modalLink.disabled = true
-  }, 300)
+    modalLink.disabled = true;
+  }, 300);
 }
-function abrirModal () {
-  modalLink.disabled = false
+function abrirModal() {
+  modalLink.disabled = false;
+  console.log(contenedorModal.clientHeight);
   setTimeout(() => {
-    let heigthModal = contenedorModal.clientHeight
-    window.scrollBy(0, -window.scrollY)
+    console.log(contenedorModal.clientHeight + " --modal");
+    console.log(document.body.clientHeight + " --body");
+    let heigthModal = contenedorModal.clientHeight;
+    window.scrollBy(0, -window.scrollY);
     // el translate Y es -50vh - la mitad del alto del modal
-    contenedorModal.style.top = `calc(${heigthModal / 2}px)`
-  }, 200)
+    contenedorModal.style.top = `calc(${heigthModal / 2}px)`;
+  }, 200);
 }
-btnCloseModal.onclick = cerrarModal
-btnMenu.onclick = abrirModal
+btnCloseModal.onclick = cerrarModal;
+btnMenu.onclick = abrirModal;
 // window.onsc}roll = () => {}
